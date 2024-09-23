b64a75e41b1f2c9387c32d7faeafdd335e1f17d0
Breathe <704340916@qq.com>
2024-09-24 00:11:27 +0800

    a

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -4,23 +4,35 @@ type TableNames = 'Users' | 'Books'
 
 const prEasyD1 = new PrEasyD1<TableNames>()
 
+let res = prEasyD1
+  .table('Users')
+  .insert([{ name: 'breathe', age: 12 }])
+  .delete(['111', '222'])
+  .update({ _id: '111', name: 'aaa' }, '_id')
+  .select({
+    $match: { $or: [{ name: 'aaa', age: 22 }, { state: 2 }] },
+    $sort: { _id: 1 }
+  })
+  .end()
+console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:res`, res)
+
 // 增加
-{
-  const { query, values } = prEasyD1.post('Users', [{ name: 'breathe' }])
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:{ query, values }`, { query, values })
-}
+// {
+//   const { query, values } = prEasyD1.post('Users', [{ name: 'breathe' }])
+//   console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:{ query, values }`, { query, values })
+// }
 
-// 查询
-{
-  const { query, values } = prEasyD1.get('Users', { name: 'breathe' })
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:{ query, values }`, { query, values })
-}
+// // 查询
+// {
+//   const { query, values } = prEasyD1.get('Users', { name: 'breathe' })
+//   console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:{ query, values }`, { query, values })
+// }
 
-// 修改
-{
-  const { query, values } = prEasyD1.put('Users', { name: 'breathe' }, { id: '123' })
-  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:{ query, values }`, { query, values })
-}
+// // 修改
+// {
+//   const { query, values } = prEasyD1.put('Users', { name: 'breathe' }, { id: '123' })
+//   console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:{ query, values }`, { query, values })
+// }
 
 // 删除
 // {
--- src/d1/index.ts
+++ src/d1/index.ts
@@ -1,113 +1,242 @@
-type ReturnDBInfo = {
-  query: string
-  values: unknown[]
+interface PipelineItem {
+  /**
+   * 多条件
+   */
+  $match?: { $or: object[] }
+
+  /**
+   * 正序
+   */
+  $sort?: {
+    [key: string]: -1 | 1
+  }
+
+  /**
+   * 末尾key
+   */
+  end_id?: string
+
+  /**
+   * 末尾id
+   */
+  end_key?: '_id' | string
+
+  /**
+   * 页码
+   */
+  page?: number | string
+
+  /**
+   * 条数
+   */
+  size?: number | string
+
+  /**
+   * 连表查询
+   */
+  // lookup?: { from: 'roles'; localField: 'roles'; foreignField: 'key'; as: 'roles' }
+  lookup?: {
+    from: string
+    localField: string
+    foreignField: string
+    as: string
+  }
+
+  /**
+   * 打散数组
+   */
+  // unwind?:{ path: '$roles', preserveNullAndEmptyArrays: false } }
+  unwind?: {
+    path: string
+    preserveNullAndEmptyArrays: boolean
+  }
+
+  /**
+   * 处理字段
+   */
+  // { $project: { 'roles.created_at': 0, 'roles.created_ip': 0, 'roles.created_user_id': 0, 'roles.permissions': 0 } }
+  $project?: {
+    'roles.created_at': 0
+    'roles.created_ip': 0
+    'roles.created_user_id': 0
+    'roles.permissions': 0
+  }
 }
 
 export class PrEasyD1<T> {
+  tableName: T | string = '' // 表名
+  query = '' // sql语句
+  values: any = [] // 数据
   constructor() {}
-  /**
-   * 增加
-   * @param table 表名
-   * @param data 数据
-   * @example post('Users',[{ name:'breathe' }])
-   * @returns { query, values }
-   */
-  post = (table: T, datas: object[]): ReturnDBInfo => {
-    let values = []
-    // `INSERT INTO table_name (field1, field2) VALUES (?, ?), (?, ?)`
-    let query = `INSERT INTO ${table}`
-    // 生成第一段
-    {
-      const strArr = Object.keys(datas[0])
-      let str = strArr.join(', ') // field1, field2
-      query = `${query} (${str})`
-    }
-
-    // 生成第二段
-    {
-      let strArr = []
-      for (const data of datas) {
-        let vals = Object.values(data)
-        let str = Array(vals.length).fill('?').join(', ')
-        str = `(${str})`
-        strArr.push(str)
-        values.push(...vals)
-      }
-      let str = strArr.join(', ') // (?, ?), (?, ?)
-      query = `${query} VALUES ${str}`
-    }
-
-    return { query, values }
-  }
 
   /**
    * 查询
-   * @param table 表名
-   * @param data 数据
-   * @example get('Users',{ username:'breathe' })
-   * @returns { query, values }
+   * @param pipelineItem 聚合条件
+   * @SQL `SELECT * FROM Applications WHERE (app_name LIKE ?) OR (comment LIKE ?) LIMIT ?, ?`
+   * @example .insert([{ name: 'breathe', age: 12 }])
    */
-  get = (table: T, data: object): ReturnDBInfo => {
-    try {
-      // SELECT * FROM Users WHERE mobile = ? username = ?
-      let query = ` SELECT * FROM ${table}`
-      const values = Object.values(data)
-      if (values.length === 0) return { query, values }
+  select(pipelineItem: PipelineItem) {
+    this.query = `SELECT * FROM ${this.tableName}`
+    this.values = []
+    let { end_id, end_key = '_id', $match, $sort, page = 1, size = 10 } = pipelineItem
 
-      {
-        const keys = Object.keys(data)
-        const splitStr = ' = ?' // 分隔符
-        let strArr = Array.from(keys, (key) => `${key}${splitStr}`)
-        let str = strArr.join(' AND ') // mobile = ? username = ?
-        query = `${query} WHERE ${str}`
-      }
-      return { query, values }
-    } catch (error) {
-      console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:error`, error)
-      return { query: '', values: [] }
+    if (end_id || $match) {
+      this.query += ' WHERE'
     }
+
+    // 指定id后查询
+    if (end_id) {
+      this.values.push(`${end_id}`)
+      let str = ` ${end_key} > ?`
+      this.query += str
+    }
+
+    // 模糊查询
+    if ($match) {
+      const { $or = [] } = $match
+      let str = ''
+      for (const [index, obj] of $or.entries()) {
+        let condition = ''
+        //   WHERE app_name LINK ? AND age LINK ?
+        const items = Object.entries(obj).entries() // [{ 1: [key, val] }]
+        for (const [index, [key, val]] of items) {
+          this.values.push(`%${val}%`)
+          if (index === 0) {
+            condition += `${key} LIKE ?`
+            continue
+          }
+          condition += ` AND ${key} LIKE ?`
+        }
+        if (index === 0) {
+          str += `(${condition})`
+          continue
+        }
+        str += ` OR (${condition})`
+      }
+      this.query += end_id ? ` AND (${str})` : ` (${str})`
+    }
+
+    if ($sort) {
+      let str = ' ORDER BY'
+      let items = Object.entries($sort).entries()
+      for (const [index, [key]] of items) {
+        if (index === 0) {
+          str += ` ${key}`
+          continue
+        }
+        str += `, ${key}`
+      }
+      this.query += `${str} ASC`
+    }
+    // page size
+    {
+      page = Math.max(1, Number(page)) // 最小为1
+      size = Math.min(50, Number(size)) // 最大为50
+      let offset = Number(page - 1) * Number(size)
+      this.values.push(offset)
+
+      let limit = Number(size)
+      this.values.push(limit)
+
+      let str = ` LIMIT ?, ?`
+      this.query += str
+    }
+    return this
   }
 
   /**
-   * 修改
-   * @param table 表名
-   * @param data 数据
-   * @example put('Users',{ username:'breathe' },{id: '123'})
-   * @returns { query, values }
+   * 插入
+   * @param datas 数据
+   * @SQL `INSERT INTO table_name (field1, field2) VALUES (?, ?), (?, ?)`
+   * @example .insert([{ name: 'breathe', age: 12 }])
    */
-  put = (table: T, datas: object, whereData: object): ReturnDBInfo => {
-    try {
-      // UPDATE Users SET mobile = ? username = ? WHERE id = ? AND username = ?
-      let query = ` UPDATE ${table}`
-      const values = [...Object.values(datas), ...Object.values(whereData)]
+  insert(datas: object[]) {
+    this.query = `INSERT INTO ${this.tableName}`
+    this.values = []
 
-      {
-        const keys = Object.keys(datas)
-        const splitStr = ' = ?' // 分隔符
-        let strArr = Array.from(keys, (key) => `${key}${splitStr}`)
-        let str = strArr.join('') // mobile = ? username = ?
-        query = `${query} SET ${str}`
+    for (const [index, data] of datas.entries()) {
+      if (index === 0) {
+        const keys = Object.keys(data)
+        const str = keys.join(', ') // field1, field2
+        this.query += `(${str})`
       }
-
-      {
-        const keys = Object.keys(whereData)
-        const splitStr = ' = ?' // 分隔符
-        let strArr = Array.from(keys, (key) => `${key}${splitStr}`)
-        let str = strArr.join(' AND ') // id = ? AND username = ?
-        query = `${query} WHERE ${str}`
+      const vals = Object.keys(data)
+      const str = Array(vals.length).fill('?').join(', ')
+      this.values.push(...vals)
+      if (index === 0) {
+        this.query += ` VALUES (${str})`
+        continue
       }
-      return { query, values }
-    } catch (error) {
-      console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:error`, error)
-      return { query: '', values: [] }
+      this.query += `, (${str})`
     }
+    return this
   }
 
   /**
    *
    * @param table 表名
-   * @param data 删除的数据
-   * @example delete('Users',[{ id:'breathe' },{ username:'breathe' }])
+   * @param ids 删除的数据唯一id数组
+   * @param keyName 删除时的条件字段key 默认为_id
+   * @SQL `DELETE FROM users WHERE ID IN (1, 2, 3)`
+   * @example .delete(['111', '222'])
    */
-  // delete = (table: T, datas: object[] = []) => {}
+  delete(ids: string[], keyName = '_id') {
+    const str = Array(ids.length).fill('?').join(', ')
+    this.query = `DELETE FROM ${this.tableName} WHERE ${keyName} IN (${str})`
+    this.values = ids
+    return this
+  }
+
+  /**
+   * 更新
+   * @param data 数据
+   * @param keyName 修改时的条件字段key 默认为_id
+   * @SQL `UPDATE Users SET mobile = ?, username = ? WHERE id = ?`
+   * @example .update({ _id: '111', name: 'aaa' }, '_id')
+   */
+  update<T extends { [key: string]: unknown }>(data: T, keyName: keyof T) {
+    this.query = `UPDATE ${this.tableName}`
+    this.values = []
+    const val = data[keyName] // 记录条件
+
+    const items = Object.entries(data).entries()
+    for (const [index, [key, val]] of items) {
+      this.values.push(val)
+      if (index === 0) {
+        this.query += ` SET ${key} = ?`
+        continue
+      }
+      this.query += `,  ${key} = ?`
+    }
+    this.query += `WHERE ${String(keyName)} = ?`
+    this.values.push(val)
+    return this
+  }
+
+  /**
+   * 操作的表
+   * @example .table('Users')
+   */
+  table(table: T) {
+    this.tableName = table
+    return this
+  }
+
+  /**
+   * 返回结果
+   * @param full 是否返回完整sql
+   * @example .end(true)
+   * @returns { query, values }
+   */
+  end(full = false) {
+    let query = this.query
+    let values = this.values
+    if (full) {
+      for (const val of values) {
+        query = query.replace('?', val)
+      }
+    }
+    let obj = { query, values }
+    return obj
+  }
 }
